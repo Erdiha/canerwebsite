3282e3484c5bba3e19302cc1783b6d3eb8a3deef
erdiha <erdiha@hotmail.com>
2024-11-18 09:06:53 -0800

    Add .env.local to .gitignore

--- src/app/api/stripe.js
+++ src/app/api/stripe.js
@@ -6,13 +6,11 @@ import convertToSubcurrency from '@/utils/convertToSubcurrency';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-if (process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY === undefined) {
+if (process.env.STRIPE_PUBLISHABLE_KEY === undefined) {
   throw new Error('Stripe publishable key is not defined');
 }
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
 
 export default function Page() {
   return (
